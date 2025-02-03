706eef675b99010f9e4b39e1a11fce582f5aff3c
Fahad <fahadurrehman001@gmail.com>
2025-02-03 16:14:06 +0500

    update information

--- src/components/about/ResumeArea.tsx
+++ src/components/about/ResumeArea.tsx
@@ -1,71 +1,72 @@
- 
-
 export default function ResumeArea() {
   return (
     <>
-      <div className="resume-area no-padding" id="resume">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-6 col-md-6">
-              <div className="resume-wrapper wow fadeInUp delay-0-2s">
-
-                <div className="resume-box">
-                  <span className="resume-date">2020 - Present</span>
-                  <h2>Web Developer & Business Partner (Full-Time)</h2>
-                  <span>@ Envato Marketplace</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <div className='resume-area no-padding' id='resume'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-xl-6 col-md-6'>
+              <div className='section-title section-black-title mb-40 wow fadeInUp delay-0-2s'>
+                <h2>Experiance</h2>
+              </div>
+              <div className='resume-wrapper wow fadeInUp delay-0-2s'>
+                <div className='resume-box'>
+                  <span className='resume-date'>Feb 2024 - Present</span>
+                  <h2>React JS Developer </h2>
+                  <span>@ Techesthete</span>
+                  <p>
+                    Consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi
+                    ut aliquip ex ea commodo consequat.
+                  </p>
                 </div>
 
-
-                <div className="resume-box">
-                  <span className="resume-date">2018 - 2020</span>
-                  <h2>CEO & Founder</h2>
-                  <span>@ Thememaster LTD</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div className='resume-box'>
+                  <span className='resume-date'>Mar 2023 - Jan 2024</span>
+                  <h2>Frontend Developer</h2>
+                  <span>@ DevJaco</span>
+                  <p>
+                    Consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi
+                    ut aliquip ex ea commodo consequat.
+                  </p>
                 </div>
-
-
-                <div className="resume-box">
-                  <span className="resume-date">2012 - 2028</span>
-                  <h2>Senior Designer</h2>
-                  <span>@ Google</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-
               </div>
             </div>
-            <div className="col-xl-6 col-md-6">
-              <div className="resume-wrapper wow fadeInUp delay-0-4s">
-
-                <div className="resume-box">
-                  <span className="resume-date">2020 - Present</span>
-                  <h2>Bachelor Degree of Science</h2>
-                  <span>@ University of London</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <div className='col-xl-6 col-md-6'>
+              <div className='section-title section-black-title mb-40 wow fadeInUp delay-0-2s'>
+                <h2>Education</h2>
+              </div>
+              <div className='resume-wrapper wow fadeInUp delay-0-4s'>
+                <div className='resume-box'>
+                  <span className='resume-date'>2017 - 2019</span>
+                  <h2>Masters in Information Technology</h2>
+                  <span>@ University of Central Punjab</span>
+                  <p>
+                    Consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi
+                    ut aliquip ex ea commodo consequat.
+                  </p>
                 </div>
 
-
-                <div className="resume-box">
-                  <span className="resume-date">2018 - 2020</span>
-                  <h2>Master Degree of Design</h2>
-                  <span>@ Envato Education</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div className='resume-box'>
+                  <span className='resume-date'>2015 - 2017</span>
+                  <h2>Bachelor of Science</h2>
+                  <span>@ Islamia University</span>
+                  <p>
+                    Consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi
+                    ut aliquip ex ea commodo consequat.
+                  </p>
                 </div>
-
-
-                <div className="resume-box">
-                  <span className="resume-date">2015 - 2018</span>
-                  <h2>Degree Of UI & UX</h2>
-                  <span>@ University of world</span>
-                  <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                </div>
-
               </div>
             </div>
           </div>
         </div>
       </div>
-
     </>
-  )
+  );
 }
--- src/components/home/HeroArea.tsx
+++ src/components/home/HeroArea.tsx
@@ -1,60 +1,86 @@
- 
+import Resume from "../../../public/assets/Fahad - Frontend Developer.pdf";
 export default function HeroArea() {
   return (
     <>
-      <section id="home" className="main-hero-area">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-
-              <div className="hero-content wow fadeInUp text-center delay-0-2s">
+      <section id='home' className='main-hero-area'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-12'>
+              <div className='hero-content wow fadeInUp text-center delay-0-2s'>
                 <h2>Fahad Ur Rehman</h2>
               </div>
-
             </div>
           </div>
-          <div className="row">
-            <div className="col-lg-3 pt-30">
-
-              <div className="hero-content wow fadeInUp delay-0-2s">
-                <div className="clienti-reviews">
-                  <ul className="clienti-profile">
+          <div className='row'>
+            <div className='col-lg-3 pt-30'>
+              <div className='hero-content wow fadeInUp delay-0-2s'>
+                <div className='clienti-reviews'>
+                  <ul className='clienti-profile'>
                     <li>
-                      <img className="img-fluid" src="assets/images/avatar/client (1).png" alt="client" />
+                      <img
+                        className='img-fluid'
+                        src='assets/images/avatar/client (1).png'
+                        alt='client'
+                      />
                     </li>
                     <li>
-                      <img className="img-fluid" src="assets/images/avatar/client (2).png" alt="client" />
+                      <img
+                        className='img-fluid'
+                        src='assets/images/avatar/client (2).png'
+                        alt='client'
+                      />
                     </li>
                     <li>
-                      <img className="img-fluid" src="assets/images/avatar/client (3).png " alt="client" />
+                      <img
+                        className='img-fluid'
+                        src='assets/images/avatar/client (3).png '
+                        alt='client'
+                      />
                     </li>
                     <li>
-                      <img className="img-fluid" src="assets/images/avatar/client (4).png " alt="client" />
+                      <img
+                        className='img-fluid'
+                        src='assets/images/avatar/client (4).png '
+                        alt='client'
+                      />
                     </li>
                   </ul>
-                  <div className="reviews">50+ reviews <span>(4.46 of 5)</span>
+                  <div className='reviews'>
+                    50+ reviews <span>(4.46 of 5)</span>
                     <p>Five-star reviews from my esteemed clients.</p>
                   </div>
                 </div>
               </div>
-
             </div>
-            <div className="col-lg-6">
-              <div className="hero-image">
-                <img src="assets/images/about/me.jpg" alt="" />
+            <div className='col-lg-6'>
+              <div className='hero-image'>
+                <img src='assets/images/about/me.jpg' alt='' />
               </div>
-
             </div>
-            <div className="col-lg-3 pt-30">
-              <div className="hero-content wow fadeInUp delay-0-4s">
-                <p>Hi, I’m Fahad, a dedicated Frontend Developer. Skilled in building responsive, user-friendly web applications, and mobile applications using modern frameworks.</p>
-                <a className="theme-btn" href="">Get In touch</a>
+            <div className='col-lg-3 pt-30'>
+              <div className='hero-content wow fadeInUp delay-0-4s'>
+                <p>
+                  Hi, I’m Fahad, a dedicated Frontend Developer. Skilled in
+                  building responsive, user-friendly web applications, and
+                  mobile applications using modern frameworks.
+                </p>
+                <div>
+                  <a
+                  target="blank"
+                    className='theme-btn'
+                    href='mailto:fahad00rehman@gmail.com'
+                  >
+                    Get In touch
+                  </a>
+                  <a className='theme-btn' target="blank" href={Resume}>
+                    Get Resume
+                  </a>
+                </div>
               </div>
-
             </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
